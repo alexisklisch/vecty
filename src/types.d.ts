1cd853e0a6c3a1b67745676f36635daaa3ed8d68
Alexis Fleitas Klisch <unbosquense@gmail.com>
2025-04-03 20:05:39 -0300

    Enhance VectyConfig interface to support array of FontsConfig with detailed font properties

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,19 @@
 export interface VectyConfig {
   variables?: Record<string, any>
-  fonts?: Record<string, Record<string, any>>
+  fonts?: FontsConfig[]
+}
+
+type TextWeight = 'thin' | 'light' | 'regular' | 'medium' | 'bold' | 'black'
+  | 'extrabold' | 'extralight' | 'semibold' | 'ultralight' | 'ultrabold'
+  | 'heavy' | 'extraheavy' | 'demibold' | 'demilight' | 'book' | 'normal' | number
+
+type TextStyle = 'normal' | 'italic' | 'oblique'
+type TextFormat = 'woff' | 'woff2' | 'truetype' | 'opentype' | 'embedded-opentype' | 'svg'
+
+interface FontsConfig {
+  name: string
+  weight: TextWeight
+  style?: TextStyle
+  src: ArrayBuffer
+  format?: TextFormat
 }
