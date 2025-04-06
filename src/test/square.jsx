6ed6e90937215b69d54f03b8c9740aba3da237dd
Alexis Fleitas Klisch <unbosquense@gmail.com>
2025-04-06 08:26:02 -0300

    Enhance SVG rendering by adding secondary color definitions and updating publication details; refactor feature rendering logic in square.jsx and improve test setup in vecty.test.ts

--- src/test/square.jsx
+++ src/test/square.jsx
@@ -1,63 +1,71 @@
 <svg width="1080" height="1080" viewBox="0 0 1080 1080" fill="none" xmlns="http://www.w3.org/2000/svg">
   <manifest vecty>
     <variables>
-      {{
-        "colors": {
-          "primary": {
-            "50": "#FCE8EC",
-            "100": "#FAD6DC",
-            "200": "#F5A8B6",
-            "300": "#F07F94",
-            "400": "#EC5671",
-            "500": "#E7284B",
-            "600": "#CA1737",
-            "700": "#97112A",
-            "800": "#650B1C",
-            "900": "#32060E",
-            "950": "#1B0308"
+      {
+        {
+          "colors": {
+            "primary": {
+              "50": "#FCE8EC",
+              "100": "#FAD6DC",
+              "200": "#F5A8B6",
+              "300": "#F07F94",
+              "400": "#EC5671",
+              "500": "#E7284B",
+              "600": "#CA1737",
+              "700": "#97112A",
+              "800": "#650B1C",
+              "900": "#32060E",
+              "950": "#1B0308"
+            },
+            "secondary": {
+              "50": "#FDF5E3",
+              "100": "#FAEAC2",
+              "200": "#F5D789",
+              "300": "#F0C24B",
+              "400": "#EDB322",
+              "500": "#CF9911",
+              "600": "#A57A0E",
+              "700": "#7A5B0A",
+              "800": "#553F07",
+              "900": "#2A1F04",
+              "950": "#130E02"
+            },
+            "grey": {
+              "0": "#F1F1F1",
+              "50": "#F5F5F5",
+              "100": "#EBEBEB",
+              "200": "#D9D9D9",
+              "300": "#C5C5C5",
+              "400": "#A8A8A8",
+              "500": "#8C8C8C",
+              "600": "#707070",
+              "700": "#545454",
+              "800": "#383838",
+              "900": "#1C1C1C",
+              "950": "#0F0F0F"
+            },
+            "green": {
+              "50": "#E1FFF6",
+              "100": "#C3FEEC",
+              "200": "#81FDD8",
+              "300": "#45FCC5",
+              "400": "#09FBB3",
+              "500": "#03BF86",
+              "600": "#02825C",
+              "700": "#015F43",
+              "800": "#01412E",
+              "900": "#001E15",
+              "950": "#000F0B"
+            }
           },
-          "secondary": {
-            "50": "#FDF5E3",
-            "100": "#FAEAC2",
-            "200": "#F5D789",
-            "300": "#F0C24B",
-            "400": "#EDB322",
-            "500": "#CF9911",
-            "600": "#A57A0E",
-            "700": "#7A5B0A",
-            "800": "#553F07",
-            "900": "#2A1F04",
-            "950": "#130E02"
-          },
-          "grey": {
-            "0": "#F1F1F1",
-            "50": "#F5F5F5",
-            "100": "#EBEBEB",
-            "200": "#D9D9D9",
-            "300": "#C5C5C5",
-            "400": "#A8A8A8",
-            "500": "#8C8C8C",
-            "600": "#707070",
-            "700": "#545454",
-            "800": "#383838",
-            "900": "#1C1C1C",
-            "950": "#0F0F0F"
-          },
-          "green": {
-            "50": "#E1FFF6",
-            "100": "#C3FEEC",
-            "200": "#81FDD8",
-            "300": "#45FCC5",
-            "400": "#09FBB3",
-            "500": "#03BF86",
-            "600": "#02825C",
-            "700": "#015F43",
-            "800": "#01412E",
-            "900": "#001E15",
-            "950": "#000F0B"
+          "publication": {
+            "operation": {
+              "type": "Venta",
+              "price": "Consultar"
+            }
           }
         }
-      }}
+      }
     </variables>
   </manifest>
   <defs>
@@ -73,7 +81,7 @@
     </filter>
   </defs>
 
-  {/* <image href="{{expr(user$$imagenCasa), later()}}" width="1080" height="611" preserveAspectRatio="xMidYMid slice"/> */}
+  <image href={user.imgs.photo} width="1080" height="611" preserveAspectRatio="xMidYMid slice"/>
   <g>
     <rect y="611" width="1080" height="469" fill="#eaeaea"/> {/*Fondo blanco */}
     <g> {/*Precio */}
@@ -98,7 +106,10 @@
         font-size="46"
         vecty:text-transform="uppercase"
         fill={system.colors.grey[0]}
-      >USD 220.000</text>
+      >{user.publication?.operation?.price && user.publication?.operation?.currency
+        ? `${user.publication.operation.currency} ${(user.publication.operation.price).toLocaleString('es-AR')}`
+        : system.publication.operation.price
+      }</text>
     </g>
 
     <g> {/* Info izquierda */}
@@ -106,7 +117,7 @@
         vecty:expand
         font-family="Montserrat"
         font-weight="600"
-        vecty:box="30 730 450 65"
+        vecty:box="30 730 470 65"
         font-size="30"
         vecty:line-height="5"
         fill="#383838"
@@ -115,50 +126,43 @@
 
       <g> {/* Features */}
 
-        <g> {/* Features: ambientes */}
-          <circle cx="30" cy="846" r="6" fill="#CA1737"/>
-          <text
-            vecty:expand
-            font-family="Montserrat"
-            font-weight="400"
-            vecty:box="47 830 225 26"
-            font-size="26"
-            fill="#383838"
-          >4 ambientes</text>
-        </g>
-        <g> {/* Features: dormitorios */}
-          <circle cx="30" cy="896" r="6" fill="#CA1737"/>
-          <text
-            vecty:expand
-            font-family="Montserrat"
-            font-weight="400"
-            vecty:box="47 880 225 26"
-            font-size="26"
-            fill="#383838"
-          >3 dormitorios</text>
-        </g>
-        <g> {/* Features: pileta */}
-          <circle cx="290" cy="846" r="6" fill="#CA1737"/>
-          <text
-            vecty:expand
-            font-family="Montserrat"
-            font-weight="400"
-            vecty:box="307 830 225 26"
-            font-size="26"
-            fill="#383838"
-          >Pileta</text>
-        </g>
-        <g> {/* Features: quincho */}
-          <circle cx="290" cy="896" r="6" fill="#CA1737"/>
-          <text
-            vecty:expand
-            font-family="Montserrat"
-            font-weight="400"
-            vecty:box="307 880 225 26"
-            font-size="26"
-            fill="#383838"
-          >Quincho</text>
-        </g>
+        {
+          {
+            tag: 'g',
+            attr: {},
+            children: user.features.map((current, index) => {
+              // Distribuye las features en un array
+              const x = index % 2 === 0 ? 30 : 290;
+              const y = index < 2 ? 846 : 896;
+              const text = current;
+              const radius = 6;
+              return ({
+                tag: 'g',
+                attr: {},
+                children: [
+                  {
+                    tag: 'circle',
+                    attr: {cx: `${x}`, cy: `${y}`, r: `${radius}`, fill: system.colors.primary[600]},
+                    children: []
+                  },
+                  {
+                    tag: 'text',
+                    attr: {
+                      "vecty:expand": '',
+                      "font-family": 'Montserrat',
+                      "font-weight": "400",
+                      "vecty:box": `${x + 17} ${y - 16} 225 26`,
+                      "font-size": "26",
+                      fill: "#383838"
+                    },
+                    children: [{text}]
+                  }
+                ]
+              })
+            })
+          }
+          
+        }
 
       </g>
 
@@ -194,7 +198,8 @@
           vecty:box="105 1010 800 28"
           font-size="28"
           fill="#0F0F0F"
-        >inmobiliariapredrito@gmail.com</text>
+        >inmobiliariapredrito@gmail.com
+        </text>
       </g>
     </g>
 
@@ -204,6 +209,31 @@
       <g> {/* Logo inmobiliaria */}
         <circle cy="618" cx="900" r="100" fill="#F5F5F5"/>
         <rect y="550" x="830" width="140" height="140" fill="{{expr('lime'), later()}}" stroke="red" />
+      </g>
+
+      <g> {/* Tipo de casa y operación */}
+        <text
+          vecty:expand
+          font-family="Montserrat"
+          vecty:text-align="center"
+          font-weight="500"
+          vecty:text-transform="uppercase"
+          vecty:box="650 780 360 50"
+          font-size="46"
+          fill={system.colors.grey[50]}
+        >Casa en
+        </text>
+        <text
+          vecty:expand
+          font-family="Montserrat"
+          vecty:text-align="center"
+          font-weight="900"
+          vecty:text-transform="uppercase"
+          vecty:box="650 830 360 78"
+          font-size="78"
+          fill={system.colors.grey[50]}
+        >Venta
+        </text>
       </g>
     </g> 
   </g>
--- src/test/vecty.test.ts
+++ src/test/vecty.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest'
 import { readFile, writeFile } from 'node:fs/promises'
-import Vecty from '@/index'
+import Vecty, { fetchBase64 } from '@/index'
 
 
 const montserrat400 = await fetch('https://cdn.jsdelivr.net/fontsource/fonts/montserrat@latest/latin-400-normal.ttf')
@@ -11,16 +11,29 @@ const montserrat600 = await fetch('https://cdn.jsdelivr.net/fontsource/fonts/mon
   .then(data => data.arrayBuffer())
 const montserrat700 = await fetch('https://cdn.jsdelivr.net/fontsource/fonts/montserrat@latest/latin-700-normal.ttf')
   .then(data => data.arrayBuffer())
+const montserrat900 = await fetch('https://cdn.jsdelivr.net/fontsource/fonts/montserrat@latest/latin-900-normal.ttf')
+  .then(data => data.arrayBuffer())
+const svg = await readFile(`${__dirname}/square.jsx`, { encoding: 'utf8' })
+const photo = await fetchBase64('https://dh0ny4gbd8gek.cloudfront.net/properties/ilga43vqfs/img-1?w=1280&format=avif')
 
 test('Lee un jsx y devuelve el SVG', async () => {
-  const svg = await readFile(`${__dirname}/square.jsx`, { encoding: 'utf8' })
-
 
   const vecty = new Vecty(svg, {
     variables: {
-      cuestion: 'La vida es así',
-      colors: {
-        blues: ['#123123', '#432343']
+      publication: {
+        operation: {
+          currency: 'USD',
+          price: 4396399
+        }
+      },
+      features: [
+        '5 dormitorios',
+        '4 ambientes',
+        'cochera',
+        '156 m² totales'
+      ],
+      imgs: {
+        photo
       },
       JSON
     },
@@ -44,6 +57,11 @@ test('Lee un jsx y devuelve el SVG', async () => {
         name: 'Montserrat',
         weight: 700,
         src: montserrat700
+      },
+      {
+        name: 'Montserrat',
+        weight: 900,
+        src: montserrat900
       },
     ]
   })
